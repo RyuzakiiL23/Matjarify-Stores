95aecc5c8d7673c49e8b0d42df92f1c1d00cd447
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2024-06-24 04:28:00 +0100

    NavMenu ready

--- front-end/components/NavBar/NavMenu.tsx
+++ front-end/components/NavBar/NavMenu.tsx
@@ -1,20 +1,22 @@
-'use client'
+"use client";
 import React, { useState, useEffect } from "react";
-import { FaBars } from "react-icons/fa6";
+import { FaBars, FaGithub, FaInstagram, FaXTwitter } from "react-icons/fa6";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function NavMenu() {
   const [open, setOpen] = useState(false);
 
   useEffect(() => {
     if (open) {
-      document.body.classList.add('overflow-hidden');
+      document.body.classList.add("overflow-hidden");
     } else {
-      document.body.classList.remove('overflow-hidden');
+      document.body.classList.remove("overflow-hidden");
     }
-    
+
     // Clean up the effect when the component is unmounted
     return () => {
-      document.body.classList.remove('overflow-hidden');
+      document.body.classList.remove("overflow-hidden");
     };
   }, [open]);
 
@@ -27,21 +29,115 @@ export default function NavMenu() {
       <div className={`absolute top-20 left-0 w-full h-full z-50`}>
         <div
           className={`bg-secbackground w-screen ${
-            open ? "h-screen overflow-hidden" : "h-0"
-          } overflow-hidden transition-all duration-500 ease-out fixed top-14 left-0 z-50`}
+            open ? "h-full overflow-auto " : " delay-150 h-0"
+          } transition-all  duration-300 ease-out flex flex-col justify-between fixed top-14 left-0 z-50`}
         >
-          <div className="flex flex-col gap-4 p-4">
-            <p className="font-bold cursor-pointer w-max">Trending</p>
-            <p className="font-bold cursor-pointer w-max">Popular</p>
-            <p className="font-bold cursor-pointer w-max">New</p>
-            <p className="font-bold cursor-pointer w-max">All</p>
-            <p className="font-bold cursor-pointer w-max">Blogs</p>
-            <p className="font-bold cursor-pointer w-max">Templates</p>
-            <p className="font-bold cursor-pointer w-max">Support</p>
+          <div className={`flex flex-col gap-4 p-4`}>
+            <p
+              className={` ${
+                open ? "ml-0" : "ml-[-100]"
+              } font-bold cursor-pointer w-max transition-all duration-[0.075s]`}
+            >
+              Trending
+            </p>
+            <p
+              className={` ${
+                open ? "ml-0" : "ml-[-100]"
+              } font-bold cursor-pointer w-max transition-all duration-[0.1s]`}
+            >
+              Popular
+            </p>
+            <p
+              className={` ${
+                open ? "ml-0" : "ml-[-100]"
+              } font-bold cursor-pointer w-max transition-all duration-[0.125s]`}
+            >
+              New
+            </p>
+            <p
+              className={` ${
+                open ? "ml-0" : "ml-[-100]"
+              } font-bold cursor-pointer w-max transition-all duration-[0.15s]`}
+            >
+              All
+            </p>
+            <p
+              className={` ${
+                open ? "ml-0" : "ml-[-100]"
+              } font-bold cursor-pointer w-max transition-all duration-[0.175s]`}
+            >
+              Blogs
+            </p>
+            <p
+              className={` ${
+                open ? "ml-0" : "ml-[-100]"
+              } font-bold cursor-pointer w-max transition-all duration-[0.2s]`}
+            >
+              Support
+            </p>
+            <p
+              className={` ${
+                open ? "ml-0" : "ml-[-100]"
+              } font-bold cursor-pointer w-max transition-all duration-[0.225s]`}
+            >
+              Templates
+            </p>
+          </div>
+          <div className={`${open ? "ml-0" : "ml-[-400] md:ml-[-1000]"} duration-300 p-4 pb-10`}>
+            <div className="flex flex-col md:relative md:flex-row-reverse">
+              <ul className="md:w-1/3">
+                <div
+                  className={`flex h-10 w-full gap-8 mt-10 md:mt-8 text-primary `}
+                >
+                  <FaGithub
+                    className={` cursor-pointer hover:opacity-50 transition-all duration-300 text-xl `}
+                  />
+                  <FaInstagram className="cursor-pointer hover:opacity-50 transition-all duration-300 text-xl" />
+                  <FaXTwitter className="cursor-pointer hover:opacity-50 transition-all duration-300 text-xl" />
+                </div>
+              </ul>
+              <ul className="flex flex-col text-trdbackground text-sm md:w-2/3">
+                <li
+                  className={`ulli border-b  mr-auto border-gray-700 text-sm mt-8 font-medium cursor-pointer `}
+                >
+                  307 Riad Al Ismailia Anasi, Meknes 50050
+                </li>
+              </ul>
+            </div>
+            <div className="flex flex-col md:flex-row md:relative md:items-center">
+              <ul className="flex text-sm gap-8 text-gray-500 md:w-2/3">
+                <li className="ulli border-b border-border text-sm md:mt-0 mt-8 cursor-pointer">
+                  <Link href={"/privacy"}>Privacy</Link>{" "}
+                </li>
+                <li className="ulli border-b border-border text-sm md:mt-0 mt-8 cursor-pointer">
+                  <Link href={"/politics"}>Politics</Link>
+                </li>
+                <li className="ulli border-b border-border text-sm md:mt-0 mt-8 cursor-pointer">
+                  <Link href={"/terms"}>Terms</Link>
+                </li>
+              </ul>
+              <div className="py-6 md:w-1/3 flex items-center gap-2">
+                <p className="text-gray-500 text-sm font-bold ">Powered by </p>
+                <Link
+                  href={"https://matjarify.vercel.app"}
+                  target="_blank"
+                  className="flex gap-2 hover:opacity-80 duration-150 ease-in-out"
+                >
+                  <div className="rounded-full bg-primary p-0.5">
+                    <Image
+                      src="/logo-no-bg.png"
+                      width={20}
+                      height={20}
+                      alt="M"
+                    />
+                  </div>
+                  <p className="text-primary text-md font-bold">Matjarify</p>
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
 }
-
