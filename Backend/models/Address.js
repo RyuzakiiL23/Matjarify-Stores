08ae27a51971ad6ab788d240d9e83fa6ec70e95a
hamza-25 <me677499@gmail.com>
2024-06-24 11:30:02 +0100

    add address model

---
+++ Backend/models/Address.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Address = sequelize.define('Address', {
+    street: {
+        type: DataTypes.STRING(60)
+    },
+    city: {
+        type: DataTypes.STRING(30)
+    },
+    country: {
+        type: DataTypes.STRING(30)
+    },
+    zip_code: {
+        type: DataTypes.STRING(30)
+    },
+    province: {
+        type: DataTypes.STRING(30)
+    },
+    phone: {
+        type: DataTypes.STRING(30)
+    },
+    user_id: {
+        type:  DataTypes.INTEGER
+    }
+}, {
+    tableName: 'addresses'
+});
+
+module.exports = Address;
