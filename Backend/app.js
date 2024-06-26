77f067bad4b79a6c02fcb3d978abb5203a8ad75d
hamza-25 <me677499@gmail.com>
2024-06-26 18:55:18 +0100

    add cors in localhost:3000

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const { sequelize, User, Store, Category } = require('./models');
+const cors = require('cors')
 const authRoute = require('./routes/authRoute');
-
-
 require('dotenv').config();
+const parseCookie = require('cookie-parser');
+
+
 const app = express();
 const APP_PORT = process.env.PORT || 8000;
 const migrate = process.argv.slice(2)[0];
 
+if (migrate === 'migrate'){
+  sequelize.sync({ alter: true }).then(() => {
+       console.log('Database & tables created!');
+     });
+}
+
+app.use(cors());
+app.use(parseCookie());
 app.use(express.json());
 
 app.use('/auth', authRoute);
@@ -15,12 +25,6 @@ app.use('/auth', authRoute);
 app.get('*', async (_req, res) => {
   return res.status(401).json({message: 'No URI for that!'});
 });
-
-if (migrate === 'migrate'){
-  sequelize.sync({ alter: true }).then(() => {
-       console.log('Database & tables created!');
-     });
-}
 
 app.listen(APP_PORT, () => {
   console.log(`Server is running on port ${APP_PORT}`);
