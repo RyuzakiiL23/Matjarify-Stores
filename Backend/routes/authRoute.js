dca98337ea1d768a0a83d32d98c065230024b7d2
hamza-25 <me677499@gmail.com>
2024-06-29 00:35:51 +0100

    add o auth facebook files

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -12,6 +12,7 @@ router.get('/logout', authController.logout);
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), authController.googleAuth);
 
+
 router.get('/facebook', passport.authenticate('facebook', { scope: ["email"] }));
 router.get('/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/' }), authController.facebookAuth);
 
